20318fa24c36f7a70ea4b4804b5f377572d0e7f8
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-01-25 11:51:47 -0300

    BUG do body corrigido

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './assets/goals.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="containerMain">
+      <section className="containerApresentation">
+        <h1 className="title">
+          Maximize sua produtividade, <br />
+          organize seu tempo e<br />
+          conquiste suas metas com Zyron.<br />
+        </h1>
+        <image src={image} alt="teste" width="800" height="800" />
+      </section>
+      <section className="containerLogin"></section>
+    </main>
   );
 }
 
