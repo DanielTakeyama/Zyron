d0b8918d19e511c0bc22bdbe5ba7d44f86088182
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-01-28 14:57:30 -0300

    Pagina de login finalizada, esta funcionando 100% responsiva, agora falta somente adicionar as informações do back-end como por exemplo: Para onde vai enviar as informações inseridas no formulário e qual será o tipo de requisição HTTP que será utilizada. Também esta faltando criar a página de registro do usuário.

--- src/App.js
+++ src/App.js
@@ -7,22 +7,28 @@ import Form from './components/organisms/forms/formLogin.js';
 function App() {
   return (
     <main className="containerMain">
+
       <section className="containerApresentation shadowDefault">
+
         <section className="titleContentApresentation">
           <h1 className="title fontH1">
               {locales.headersApp.headerApresentationPartOne}<br />
               {locales.headersApp.headerApresentationPartTwo}<br />
               {locales.headersApp.headerApresentationPartThree}<br />
           </h1>
-        </section>
+        </section>{/*Fim do titleContentApresentation*/}
+
         <section className="contentImageApresentation">
           <img className="imageApresentation" src={image} alt="Gráfico de metas"/>
-        </section>
-      </section>
+        </section>{/*Fim do contentImageApresentation*/}
+
+      </section>{/*Fim do containerApresentation*/}
+
       <section className="containerLogin">
         <Form />
-      </section>
-    </main>
+      </section>{/*Fim do containerLogin*/}
+
+    </main>//Fim do containerMain
   );
 }
 
