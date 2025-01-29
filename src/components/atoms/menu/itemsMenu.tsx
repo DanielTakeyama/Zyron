f770718a2abcad3f331eb459d90ffdec996030d4
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-01-29 12:40:24 -0300

    Iniciando o desenvolvimento do NAVBAR do dashboard

--- src/components/atoms/menu/itemsMenu.tsx
+++ src/components/atoms/menu/itemsMenu.tsx
@@ -10,7 +10,7 @@ interface itemsMenuProps{
 export default function ItemsMenu({param}:itemsMenuProps){
     const dadosMenu = ["Mercado", "Lembretes", "Faculdade", "Tarefas", "Pesquisas", "testes", "projetos", "Analise"];
     const dados = dadosMenu.map((dado) => 
-        <li className={param === dado ? "fontMenu active" : "fontMenu"}>
+        <li key={dado} className={param === dado ? "fontMenu active" : "fontMenu"}>
             <img className="iconList" src={iconList} alt="icon list" width="30" height="30"/>
             <Link className="itemMenu" to={"?list=" + dado}>{dado}</Link>
         </li>
--- src/components/pages/dashboard/dashboard.jsx
+++ src/components/pages/dashboard/dashboard.jsx
@@ -1,5 +1,11 @@
+import './dashboard.css'
 import Menu from '../../molecules/sideMenu/sideMenu';
 
 export default function Dashboard(){
-    return <Menu />
+    return (
+        <main className="containerDashboard">
+            <Menu />
+            <h1>teste</h1>
+        </main>
+    );
 }
