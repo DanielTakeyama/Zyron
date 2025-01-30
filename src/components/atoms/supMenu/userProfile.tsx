9b51264b5dd3186c5fd0b169839728cae5fc8e0b
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-01-30 13:27:53 -0300

    Iniciando o userProfile.tsx

--- src/components/atoms/supMenu/userProfile.tsx
+++ src/components/atoms/supMenu/userProfile.tsx
@@ -1,4 +1,6 @@
 import './userProfile.css';
+import '../../../styles/base.css'
+import ImagePlaceholder from '../../../assets/images/placeholder_img.png';
 import React from 'react';
 
 interface userProfileProps{
@@ -8,11 +10,14 @@ interface userProfileProps{
 export default function UserProfile( { name }:userProfileProps ){
     return(
         <section className="containerUserProfile">
-
+            
             <section className="containerDataUserProfile">
+                <h3 className="fontH3 nameUser">{name}</h3>
+                <p>Meu perfil</p>
             </section>
 
             <section className="containerImageUserProfile">
+                <img className="imageUser" src={ImagePlaceholder} alt="Imagem de Perfil"/>
             </section>
 
         </section>
