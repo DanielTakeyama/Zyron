656bc9279a0dbf3add210b653f9d8cd6f67e42a9
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-02-10 15:05:29 -0300

    Base do pop-up de criação de lista feita com sucesso

--- src/components/molecules/popUp/createList/dataCreateList.jsx
+++ src/components/molecules/popUp/createList/dataCreateList.jsx
@@ -1,24 +1,32 @@
 import './dataCreateList.css';
 import '../../../../styles/base.css';
 import jsonText from '../../../../locales/locales.json';
+import Button from '../../../atoms/buttons/buttonDefault.tsx';
+import { useEffect } from 'react';
 
 export default function DataCreateList(){
+    useEffect(()=>{
+        let closeBtn = document.getElementById("closeCreateList");
+        if(closeBtn){
+            closeBtn.addEventListener("click", ()=>{
+                alert("Hello, world!");
+            });
+        }
+    }, []);
     return (
         <main className="containerDataCreateList">
             <section className="containerTitleCreateList">
                 <h2 className="fontH2 colorDefaultText">{jsonText.createList.title}</h2>
+                <h2 className="fontH2" id="closeCreateList">X</h2>
             </section>
             <hr/>
             <form className="containerContentCreateList" method="POST" action="">  
-                <h3 className="fontH3 colorDefaultText">Nome</h3>
-                <input className="inputTextFormCreateList inputNameList" type="text" id="nameList" name="nameList" placeholder="Ex: Mercado.." required></input>
-
-                <h3 className="fontH3 colorDefaultText">Tipo</h3>
-                <input className="inputTextFormCreateList inputNameList" type="text" id="nameList" name="nameList" placeholder="Ex: Pública - Somente eu posso ver" required></input>
-
-                <h3 className="fontH3 colorDefaultText">Compartilhar com</h3>
-                <input className="inputTextFormCreateList inputNameList" type="text" id="nameList" name="nameList" placeholder="Ex: DanielTakeyama" required></input>
-            </form>
+                <section className="containerContentFormCreateList">
+                    <h3 className="fontH3 colorDefaultText titleInputName">Nome</h3>
+                    <input className="inputTextFormCreateList inputNameList shadowDefault" type="text" id="nameList" name="nameList" placeholder="Ex: Mercado.." required></input>
+                    <Button onClick="" text="Cadastrar Lista"/>
+                </section>
+            </form> 
         </main>
     );
 }
